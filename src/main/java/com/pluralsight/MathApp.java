82fe7f4b659e0ecf1b11db96518854eefd49e9bd
KyrraRose <krbosland1@gmail.com>
2025-09-24 10:33:05 -0400

    Finished the rest of the questions with this one!

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -20,8 +20,35 @@ public class MathApp {
         System.out.println("The area of a circle with a radius of "+radius+" is about "+area+".");
 
         //Question 4
-        double number = 5;
+        int number = 5;
         double squareRoot = Math.sqrt(number);
         System.out.println("The square root of "+number+" is "+squareRoot+".");
+
+        //Question 5
+        //Point A (5,10) Point B (85,50)
+        int pointAX = 5;
+        int pointAY = 10;
+        int pointBX = 85;
+        int pointBY = 50;
+        int pointX = pointBX - pointAX;
+        int pointY = pointBY - pointAY;
+        double distance = Math.sqrt((pointX*pointX)+(pointY*pointY));
+        System.out.println("The distance between point A and point B is "+distance);
+
+        //Question 6
+        float negativeNumber = -3.8f;
+        double absolute = Math.abs(negativeNumber);
+        System.out.println("The absolute of "+negativeNumber+" is "+absolute+".");
+
+        //Question 7
+        double randomNumber = Math.random();
+        System.out.println("Your number is "+randomNumber+"!");
+
+        //Question 8
+        int dayNumber = 24;
+        int dayMinutes = (dayNumber*24)*60;
+        double dayMilliseconds = dayMinutes*60000;
+        System.out.println(dayNumber+" days have "+dayMinutes+" minutes; or  "+dayMilliseconds+" milliseconds.");
+
     }
 }
